82ee2874a289a1c033f258598ebfea24e8b2b82c
asifhossain3131 <asifhossain3131@gmail.com>
2024-02-07 16:03:29 +0400

    service dashboard added

--- src/pages/dashboard pages/admin/services/AdminServiceControlTable.tsx
+++ src/pages/dashboard pages/admin/services/AdminServiceControlTable.tsx
@@ -8,23 +8,49 @@ import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import DisplaySettingsIcon from '@mui/icons-material/DisplaySettings';
 import PendingActionsIcon from '@mui/icons-material/PendingActions';
+import { Menu, MenuItem } from '@mui/material';
+import axios from 'axios';
 
 
 
 type ServiceType={
-    services:[]
+    services:[],
+    refetch:any
 }
-const AdminServiceControlTable = ({services}:ServiceType) => {
+const AdminServiceControlTable = ({services,refetch}:ServiceType) => {
+  const [modalOpen,setModalOpen]=React.useState(false)
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null)
+  };
+
+  const handleDeleteService=async(id:string)=>{
+ try {
+  const res=await axios.delete(`http://localhost:5000/allServices`,{data:id})
+  if(res.data.success){
+    refetch()
+    setModalOpen(!modalOpen)
+  }
+ } catch (error) {
+  console.log(error)
+ }
+  }
+
     return (
         <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <Table sx={{ maxWidth: 1000 }} aria-label="simple table">
           <TableHead>
             <TableRow>
               <TableCell></TableCell>
               <TableCell align="left">Service</TableCell>
               <TableCell align="left">Service Details</TableCell>
               <TableCell align="right">Check it out</TableCell>
-              <TableCell align="right">Actions</TableCell>
+              <TableCell align="right">Actions
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -39,7 +65,20 @@ const AdminServiceControlTable = ({services}:ServiceType) => {
                 <TableCell align="left">{service_title}</TableCell>
                 <TableCell align="left">{description||'Not available'}</TableCell>
                 <TableCell align="right"><button><DisplaySettingsIcon></DisplaySettingsIcon></button></TableCell>
-                <TableCell align="right"><button><PendingActionsIcon></PendingActionsIcon></button></TableCell>
+                <TableCell align="right"><button onClick={handleClick}><PendingActionsIcon></PendingActionsIcon></button>
+                <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem ><button onClick={()=>setModalOpen(!modalOpen)}>Update</button></MenuItem>
+        <MenuItem><button onClick={()=>handleDeleteService(_id)}>Delete</button></MenuItem>
+      </Menu>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
--- src/pages/dashboard pages/admin/services/AdminServicesControl.tsx
+++ src/pages/dashboard pages/admin/services/AdminServicesControl.tsx
@@ -1,11 +1,16 @@
+import { Fab } from "@mui/material";
 import useServices from "../../../../hooks/useServices";
 import AdminServiceControlTable from "./AdminServiceControlTable";
+import AddIcon from '@mui/icons-material/Add';
 
 const AdminServicesControl = () => {
-    const {services}=useServices()
+    const {services,refetch}=useServices()
     return (
-        <div className="max-w-[1000px] bg-red-600 mx-auto mt-12">
-           <AdminServiceControlTable services={services}></AdminServiceControlTable>
+        <div className="max-w-[1000px] mx-auto mt-12">
+             <Fab style={{marginBottom:'10px'}} color="primary" aria-label="add">
+        <AddIcon />
+      </Fab>
+           <AdminServiceControlTable services={services} refetch={refetch}></AdminServiceControlTable>
         </div>
     );
 };
